5ec0a79d02f473774ce4ecbe6eab9c1fca6baa9d
jesse318s <jesse318s@gmail.com>
2024-04-12 20:53:07 -0500

    refactor app test prototype

--- src/app.test.jsx
+++ src/app.test.jsx
@@ -1,71 +1,82 @@
 import { render, fireEvent, screen, waitFor } from "@testing-library/react";
+import { serializedNeuralNetwork } from "./constants/serializedNeuralNetwork";
 import App, { neuralNetwork } from "./App";
+
+jest.mock("./constants/serializedNeuralNetwork", () => ({
+  serializedNeuralNetwork: {
+    run: jest.fn().mockReturnValue({ price: 0.04594 }),
+  },
+}));
 
 jest.mock("brain.js", () => {
   return {
     NeuralNetwork: jest.fn().mockImplementation(() => {
       return {
         train: jest.fn(),
-        run: jest.fn().mockReturnValue({ price: 0.09 }),
+        run: jest.fn().mockReturnValue({ price: 0.05088 }),
       };
     }),
   };
 });
 
 describe("App", () => {
-  beforeEach(() => render(<App />));
+  let predictPriceButton;
+  let trainingModeCheckbox;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    render(<App />);
+    predictPriceButton = screen.getByText("Predict Price");
+    trainingModeCheckbox = screen.getByText("Training Mode:");
+  });
 
   test("Predict Price button is rendered", () => {
-    const buttonElement = screen.getByText("Predict Price");
-
-    expect(buttonElement).toBeInTheDocument();
+    expect(predictPriceButton).toBeInTheDocument();
   });
 
-  test("Training Mode checkbox is rendered", () => {
-    const checkboxElement = screen.getByText("Training Mode:");
+  test("Predict Price button functions as expected in Performance Mode", async () => {
+    fireEvent.click(predictPriceButton);
 
-    expect(checkboxElement).toBeInTheDocument();
-  });
+    await waitFor(() => {
+      expect(serializedNeuralNetwork.run).toHaveBeenCalled();
+    });
 
-  test("Predict Price button functions as expected in Performance Mode", () => {
-    const buttonElement = screen.getByText("Predict Price");
-    const h3Element = screen.getByText("Price: $0");
-
-    fireEvent.click(buttonElement);
-
-    waitFor(() => {
-      expect(h3Element).toHaveTextContent("Price: $45.94");
+    await waitFor(() => {
+      const updatedPriceHeading = screen.getByText("Price: $45.94");
+      expect(updatedPriceHeading).toBeInTheDocument();
     });
   });
 
-  test("Predict Price button functions as expected in Training Mode", async () => {
-    const checkboxElement = screen.getByText("Training Mode:");
-    const buttonElement = screen.getByText("Predict Price");
-    const h3Element = screen.getByText("Price: $0");
+  test("Training Mode checkbox is rendered", () => {
+    expect(trainingModeCheckbox).toBeInTheDocument();
+  });
 
-    fireEvent.click(checkboxElement);
-    fireEvent.click(buttonElement);
+  test("Predict Price button functions as expected in Training Mode", async () => {
+    fireEvent.click(trainingModeCheckbox);
+    fireEvent.click(predictPriceButton);
 
     await waitFor(() => {
       expect(neuralNetwork.train).toHaveBeenCalled();
     });
 
-    expect(neuralNetwork.run).toHaveBeenCalledWith({
-      year: 0.1977,
-      genre_Action: 0,
-      genre_Adventure: 0,
-      genre_RPG: 0,
-      genre_Simulation: 0,
-      genre_Strategy: 0,
-      genre_Sports: 0,
-      genre_Puzzle: 0,
-      platform_Console: 0,
-      platform_PC: 0,
+    await waitFor(() => {
+      expect(neuralNetwork.run).toHaveBeenCalledWith({
+        year: 0.1977,
+        genre_Action: 0,
+        genre_Adventure: 0,
+        genre_RPG: 0,
+        genre_Simulation: 0,
+        genre_Strategy: 0,
+        genre_Sports: 0,
+        genre_Puzzle: 0,
+        platform_Console: 0,
+        platform_PC: 0,
+      });
     });
-    expect(neuralNetwork.run()).toStrictEqual({ price: 0.09 });
 
-    waitFor(() => {
-      expect(h3Element).toHaveTextContent("Price: $90.00");
+    await waitFor(() => {
+      const updatedPriceHeading = screen.getByText("Price: $50.88");
+      expect(updatedPriceHeading).toBeInTheDocument();
     });
   });
 });
